5e805cc380de431699ff5a40902d9f54bc449f72
tmvf <tmvf@yandex.ru>
2018-10-19 18:04:30 +0300

    07

--- src/main/resources/static/ut07/webpack.config.js
+++ src/main/resources/static/ut07/webpack.config.js
@@ -1,6 +1,13 @@
+
 const path = require("path");
 const webpack = require("webpack");
-const ExtractTextPlugin= require("extract-text-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ImageminPlugin = require("imagemin-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var isProduction = (process.env.NODE_ENV === "production");
 
 
 module.exports = {
@@ -12,39 +19,57 @@ module.exports = {
         ]
     },
     output: {
-        filename:"js/[name].js",
+        filename: "js/[name].js",
         path: path.resolve(__dirname, "dist"),
         publicPath: "../"
     },
-    devServer:{
+    devServer: {
         contentBase: "./app"
     },
 
-    module:{
+    devtool: (isProduction) ? "" : "inline-source-map",
+    module: {
         rules: [
             {
                 test: /\.scss$/,
                 use: ExtractTextPlugin.extract({
-                    use:[
+                    use: [
                         {
-                            loader:"css-loader",
-                            options:{sourceMap: true}
+                            loader: "css-loader",
+                            options: {sourceMap: true}
                         },
                         {
-                            loader:"sass-loader",
-                            options:{sourceMap: true}
+                            loader: "postcss-loader",
+                            options: {sourceMap: true}
+                        },
+                        {
+                            loader: "sass-loader",
+                            options: {sourceMap: true}
                         }
                     ],
                     fallback: "style-loader"
 
                 })
+            },
+            {
+                test: /\.(png|gif|jpe?g)$/,
+                loader: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[path[name].[ext]"
+                        }
+                    },
+                    "img-loader"
+                ]
             }
         ]
     },
-    plugins:[
+    plugins: [
         new ExtractTextPlugin(
             "./css/[name].css"
         )
     ]
-};
+}
+;
 
---
+++ src/main/resources/static/ut09/src/index.js
@@ -0,0 +1,9 @@
+// import $ from "jquery"
+import avg from "./some.js";
+
+// $(".title").html("some text");
+console.log(avg.avg(1, 2, 3, 5, 6));
+console.log(avg.max(1, 2, 33, 5, 6));
+console.log(avg.marge({s:1},{d:3}));
+
+import "./css/style.css";
