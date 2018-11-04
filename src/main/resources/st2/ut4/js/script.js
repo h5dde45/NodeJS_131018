83edb4dff06dc9d0610056bef7f011571a85a7ea
tmvf <tmvf@yandex.ru>
2018-11-04 13:48:21 +0300

    19

--- src/main/resources/st2/ut4/js/script.js
+++ src/main/resources/st2/ut4/js/script.js
@@ -1,13 +1,61 @@
 new Vue({
     el: "#app",
     data: {
-        name: "asd",
-        checked: false
+        tasks: [
+            {
+                text: "qwe",
+                isCompleted: false
+            },
+            {
+                text: "sad",
+                isCompleted: false
+            },
+            {
+                text: "zxc",
+                isCompleted: true
+            }
+        ],
+        currentTask: "",
+        editValue: "",
+        isEditing: false
     },
-    config:{
-
-    },
-    methods:{
-
+    computed: {},
+    methods: {
+        addTask(){
+            this.tasks.push({
+                text: this.currentTask,
+                isCompleted: false
+            });
+            this.currentTask = ""
+        },
+        removeTask(taskText){
+            this.tasks = this.tasks.filter(
+                task => {
+                    return task.text != taskText;
+                }
+            )
+        },
+        changeEdit(taskText){
+            this.editValue = taskText;
+            this.tasks = this.tasks.map(
+                task => {
+                    if (task.text === taskText) {
+                        task.isEditing = !task.isEditing
+                    }
+                    return task;
+                }
+            )
+        },
+        editTask(taskText){
+            this.tasks = this.tasks.map(
+                task => {
+                    if (task.text === taskText) {
+                        task.isEditing = !task.isEditing;
+                        task.text=this.editValue;
+                    }
+                    return task;
+                }
+            )
+        },
     }
 });
