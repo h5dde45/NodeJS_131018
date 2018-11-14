07c659d7228d9a824190d112b8894bc7f2b1d56b
tmvf <tmvf@yandex.ru>
2018-11-14 21:45:55 +0300

    24

--- src/main/resources/st2/ut7/script.js
+++ src/main/resources/st2/ut7/script.js
@@ -1,24 +1,20 @@
-let tttt = new Vue({
+let app = new Vue({
     el: "#app",
     data: {
-        counter: 0,
-        title: ""
+        title: "Asd"
+    },
+    methods: {},
+    computed: {}
+});
+let app2 = new Vue({
+    el: "#app2",
+    data: {
+        title: "Asd"
     },
     methods: {
-        plus(n, n1){
-            this.counter += n;
-            this.title = "+ " + n + ":";
-            if (n === 5) {
-                n1.target.style.color = "blue"
-            } else {
-                n1.target.style.color = "green"
-            }
-        },
-        minus(){
-            this.counter--;
-        },
-        newColor(p){
-            p.target.style.background = 'red'
+        update(){
+            app.title = "zxc";
         }
-    }
+    },
+    computed: {}
 });
